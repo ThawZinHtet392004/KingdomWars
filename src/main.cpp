bec50d05ef3ddf1d486fd757c000ecb0c53bf093
vijayashankarmohith2 <68358643+MV487@users.noreply.github.com>
2024-03-16 20:06:13 -0700

    refactored code and created unit.cpp unit.h

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+using namespace std;
+//Unit::Unit():health(0),attack(0),defense(0),intel(0),rank(){
+//	cout << "Inside default unit constructor: ";
+//}
+
+
+void win()
+{
+	
+}
+
+int main()
+{
+
+
+	system("Pause");
+	return 0;
+}
+
